c9874f037378741f9d812ffb7c4051c026eecac9
KeitaShira <a5818047@aoyama.jp>
2021-06-30 15:52:34 +0900

    ok

---
+++ Sample.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print(u"整数を入れてください")
+x=int(input(":"))
+if x<0:
+    print(u"負数")
+elif x==0:
+    print(u"ゼロ")
+else:
+    print(u"整数")
---
+++ ristniho.py
@@ -0,0 +1,2 @@
+vec=[2,4,6]
+[[x,x**2] for x in vec]
